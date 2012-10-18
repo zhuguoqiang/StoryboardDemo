b539254a0dcd4d434607fda1eef323f3f0c45599
Apple002 <apple002@localhost.(none)>
2012-10-18 18:15:04 +0800

    Initial Commit

---
+++ StoryboardDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryboardDemo
+//
+//  Created by Apple002 on 12-10-18.
+//  Copyright (c) 2012年 Apple002. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoryboardDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoryboardDemo
+//
+//  Created by Apple002 on 12-10-18.
+//  Copyright (c) 2012年 Apple002. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
